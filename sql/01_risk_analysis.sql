27dfb232528acfd3f81267251f9bb22cc324b570
Tol_Ber <berezovchuk1010@mail.ru>
2026-03-26 23:04:23 +0300

    Update 01_risk_analysis.sql

--- sql/01_risk_analysis.sql
+++ sql/01_risk_analysis.sql
@@ -1,15 +1,16 @@
+--Top‑20 High‑Risk Professions
 SELECT job_role, industry, automation_risk_percent
 FROM ai_jobs
 ORDER BY automation_risk_percent DESC
 LIMIT 20;
 
-
+--Average Automation Risk by Industry
 SELECT industry, AVG(automation_risk_percent) AS avg_risk
 FROM ai_jobs
 GROUP BY industry
 ORDER BY avg_risk DESC;
 
-
+--Average Automation Risk by Country
 SELECT country, AVG(automation_risk_percent) AS avg_risk
 FROM ai_jobs
 GROUP BY country
